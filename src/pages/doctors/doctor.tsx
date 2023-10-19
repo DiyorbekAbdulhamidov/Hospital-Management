e51f4a4a01ca3bc624e7ed3e9e8b5f125f3b3e63
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-10-19 14:39:15 +0500

    finish booking doctors

--- src/pages/doctors/doctor.tsx
+++ src/pages/doctors/doctor.tsx
@@ -41,8 +41,8 @@ const Doctor: FunctionComponent<DoctorProps> = () => {
       );
 
       if (response.status === 200) {
-        console.log('Times:', response.data);
-        setBookingTimes(response.data.data);
+        const timesWithSelection = response.data.data.map((time: any) => ({ ...time, selected: false }));
+        setBookingTimes(timesWithSelection);
       }
     }
     catch (error: any) {
@@ -77,6 +77,36 @@ const Doctor: FunctionComponent<DoctorProps> = () => {
     }
     getDoctorData();
   }, [doctorId, token]);
+
+  const bookAppointment = async (selectedTimeSlot: any) => {
+    if (!selectedTimeSlot) {
+      alert.error('Please select a time slot for booking.');
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        'http://134.209.20.129:8084/hybrid-booking/save',
+        {
+          timeSlotId: selectedTimeSlot.id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        alert.success('Appointment booked successfully.');
+        close();
+      }
+    }
+    catch (error: any) {
+      alert.error('Error: ' + error.message);
+      console.error('Xatolik yuz berdi: ', error);
+    }
+  };
 
   return (
     <Box p={20} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 30 }}>
@@ -121,14 +151,25 @@ const Doctor: FunctionComponent<DoctorProps> = () => {
       </Button>
 
       <Modal opened={opened} onClose={close} size="auto" sx={{ borderRadius: 20 }}>
-        <Text size={30} align='center'>{doctorData?.fullName}`s awaibale times</Text>
-        <Group mt="" >
+        <Text mb={20} size={24} color='black' align='center'>{doctorData?.fullName}'s available times:</Text>
+        <Group sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
           {bookingTimes?.map((times: any) => (
-            <Button key={times.id} style={{ margin: '5px' }}> {times.bookingTime}</Button>
+            <Button
+              key={times.id}
+              style={{ margin: '5px' }}
+              color={times.selected ? 'green' : 'blue'}
+              onClick={() => { const updatedTimes = bookingTimes.map((time) => ({ ...time, selected: time.id === times.id ? !times.selected : false, })); setBookingTimes(updatedTimes); }}
+            >
+              {times.bookingTime}
+            </Button>
           ))}
         </Group>
+        <Button size='40' left={100} w={130} h={40} color='red'
+          mt={20}
+          onClick={() => bookAppointment(bookingTimes?.find((time: any) => time.selected))}>
+          Booking
+        </Button>
       </Modal>
-
     </Box>
   );
 };
